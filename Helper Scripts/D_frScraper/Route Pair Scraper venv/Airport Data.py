42ddd0cb08b69a162e22a17923a999316804febf
Kyara <84278420+Kya-ra@users.noreply.github.com>
2024-03-19 23:21:00 +0000

    improved automated handling

--- Helper Scripts/D_frScraper/Route Pair Scraper venv/Airport Data.py
+++ Helper Scripts/D_frScraper/Route Pair Scraper venv/Airport Data.py
@@ -18,7 +18,7 @@ session1.headers = headers1
 
 iata = open("dataneeded.csv", "r")
 output = open("output.csv", "a")
-airports = ['AAA', 'LHR', 'QQQ', 'SIN', 'SYD']
+airports = []
 for i in iata:
     airports.append(i.strip())
 iata.close()
@@ -27,6 +27,7 @@ for i in airports:
     response = session1.get(f"http://www.gcmap.com/airport/{i}")
     content = response.content.decode('utf-8', errors='ignore')
     soup = BeautifulSoup(content, 'html.parser')
+    print(i)
     if response.history:
         output.write(f",,,,{i},," + "\n")
     elif response.status_code == 200:
@@ -35,8 +36,12 @@ for i in airports:
         citySection = str(trTables[4].find_all(class_='locality'))[24:-8]
         countrySection = str(trTables[4].find_all(class_='country-name'))[28:-8]
         nameSection = str(trTables[7].find_all(class_='fn org'))[32:-6]
-        latSection = str(trTables[13].find_all(class_='latitude'))[31:-10]
-        longSection = str(trTables[14].find_all(class_="longitude"))[32:-10]
+        try:
+            latSection = str(trTables[13].find_all(class_='latitude'))[31:-10]
+            longSection = str(trTables[14].find_all(class_="longitude"))[32:-10]
+        except:
+            latSection = ""
+            longSection = ""
         output.write(f"{nameSection},{citySection},,{countrySection},{i},{latSection},{longSection}" + "\n")
 
     else:
